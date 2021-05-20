b546988552e335a48b03731d9435a37cfdb1f7f4
ipa-nhg <nadia.hammoudeh.garcia@ipa.fraunhofer.de>
2021-05-20 15:01:10 +0200

    add services to the example

--- test_ros2/src/test_ros2.cpp
+++ test_ros2/src/test_ros2.cpp
@@ -6,10 +6,10 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "rcutils/cmdline_parser.h"
-#include <std_msgs/msg/string.hpp>
+#include <sensor_msgs/msg/laser_scan.hpp>
 #include <sensor_msgs/msg/battery_state.hpp>
-#include <std_msgs/msg/bool.hpp>
-#include <sensor_msgs/msg/image.hpp>
+#include <std_srvs/srv/set_bool.hpp>
+#include <std_srvs/srv/trigger.hpp>
 
 using namespace std::chrono_literals;
 using std::placeholders::_1;
@@ -27,44 +27,51 @@ void print_usage()
   class test_ros2 : public rclcpp::Node {
     public:
       test_ros2() : Node("test_ros2") {
-        string_pub_ = this->create_publisher<std_msgs::msg::String>("string_pub",10);
-        battery_pub_ = this->create_publisher<sensor_msgs::msg::BatteryState>("battery_pub",10);
-        bool_sub_ = this->create_subscription<std_msgs::msg::Bool>("bool_sub", 10, std::bind(&test_ros2::bool_sub_callback, this, _1));
-        image_sub_ = this->create_subscription<sensor_msgs::msg::Image>("image_sub", 10, std::bind(&test_ros2::image_sub_callback, this, _1));
+        scan_ = this->create_publisher<sensor_msgs::msg::LaserScan>("scan",10);
+        power_state_ = this->create_subscription<sensor_msgs::msg::BatteryState>("power_state", 10, std::bind(&test_ros2::power_state_callback, this, _1));
+        init_ = this->create_client<std_srvs::srv::Trigger>("init");
+        setBool_ = this->create_service<std_srvs::srv::SetBool>("setBool", std::bind(&test_ros2::setBool_handle, this, _1, _2, _3));
 
         timer_ = this->create_wall_timer(500ms, std::bind(&test_ros2::timer_callback, this));
 
+      	// Service client
+        while (!init_->wait_for_service(std::chrono::seconds(10))){
+          RCLCPP_ERROR(this->get_logger(), "Client interrupted while waiting for service '%s' to appear.", "init");
+        }
+        auto request = std::make_shared<std_srvs::srv::Trigger::Request>();
+        // request-> ... = ....;
+        auto result_future = init_->async_send_request(request);
+        auto result = result_future.get();
+        RCLCPP_INFO(this->get_logger(), "Service called, service: '%s'", "init");
       }
 
     private:
       // Subscriber callback
-      void bool_sub_callback(const std_msgs::msg::Bool::SharedPtr msg) const {
-        RCLCPP_INFO(this->get_logger(), "bool_sub topic got a message");
+      void power_state_callback(const sensor_msgs::msg::BatteryState::SharedPtr msg) const {
+        RCLCPP_INFO(this->get_logger(), "power_state topic got a message");
       }
 
-      rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr bool_sub_ ;
-      // Subscriber callback
-      void image_sub_callback(const sensor_msgs::msg::Image::SharedPtr msg) const {
-        RCLCPP_INFO(this->get_logger(), "image_sub topic got a message");
-      }
-
-      rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr image_sub_ ;
-      rclcpp::Publisher<std_msgs::msg::String>::SharedPtr string_pub_;
-      rclcpp::Publisher<sensor_msgs::msg::BatteryState>::SharedPtr battery_pub_;
+      rclcpp::Subscription<sensor_msgs::msg::BatteryState>::SharedPtr power_state_ ;
+      rclcpp::Publisher<sensor_msgs::msg::LaserScan>::SharedPtr scan_;
       // Timer Callback
       void timer_callback(){
-        auto string_pub_msg = std_msgs::msg::String();
-        //string_pub_msg = ...
-        string_pub_->publish(string_pub_msg);
-        RCLCPP_INFO(this->get_logger(), "string_pub publisher active");
-        auto battery_pub_msg = sensor_msgs::msg::BatteryState();
-        //battery_pub_msg = ...
-        battery_pub_->publish(battery_pub_msg);
-        RCLCPP_INFO(this->get_logger(), "battery_pub publisher active");
+        auto scan_msg = sensor_msgs::msg::LaserScan();
+        //scan_msg = ...
+        scan_->publish(scan_msg);
+        RCLCPP_INFO(this->get_logger(), "scan publisher active");
       }
       rclcpp::TimerBase::SharedPtr timer_;
 
-      
+      rclcpp::Client<std_srvs::srv::Trigger>::SharedPtr init_;
+
+      // Service Handler
+      void setBool_handle( const std::shared_ptr<rmw_request_id_t> request_header,
+        const std::shared_ptr<std_srvs::srv::SetBool::Request> request_,
+        const std::shared_ptr<std_srvs::srv::SetBool::Response> response_){
+        (void)request_header;
+        RCLCPP_INFO( this->get_logger(), "trigger service '%s'","setBool");
+      }
+      rclcpp::Service<std_srvs::srv::SetBool>::SharedPtr setBool_;
 };
 
 int main(int argc, char * argv[])
